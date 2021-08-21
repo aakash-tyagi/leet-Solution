60b8fb4c9dddbf8bb288436bb97099fff75c0326
aakash <tyagi01aakash.com>
2021-08-21 22:36:57 +0530

    length of last word

---
+++ length-of-last-word/main.go
@@ -0,0 +1,42 @@
+package main
+
+import "fmt"
+
+func lengthOfLastWord(s string) int {
+
+	a := 0
+	b := 0
+	if s == "" {
+		return 0
+	}
+
+	l := len(s)
+
+	for i := l - 1; i >= 0; i-- {
+		if string(s[i]) == " " {
+
+			a++
+		} else {
+			break
+		}
+
+	}
+
+	for i := l - 1 - a; i >= 0; i-- {
+		if string(s[i]) == " " {
+			break
+		} else {
+
+			b++
+		}
+
+	}
+
+	return b
+}
+
+func main() {
+	a := "how are you  "
+	fmt.Println(lengthOfLastWord(a))
+
+}

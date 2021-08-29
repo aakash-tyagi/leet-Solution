d7a27b1277f53be25f61f32706dc0f6325ca8de8
aakash <tyagi01aakash.com>
2021-08-29 10:38:48 +0530

    trailing-zeros

---
+++ trailing-zeros/main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"fmt"
+)
+
+func trailingZeroes(n int) int {
+	res := 0
+	for n != 0 {
+		res += n / 5
+		n /= 5
+	}
+	return res
+}
+
+func main() {
+
+	fmt.Println(trailingZeroes(15))
+}

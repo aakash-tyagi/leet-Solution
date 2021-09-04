a636908a882b2d5c9d08d93a01c09cbc30559f56
aakash <tyagi01aakash.com>
2021-09-04 10:22:44 +0530

    reverse-vowel-of-string

---
+++ reverse-vowel-of-string/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+)
+
+func reverseVowels(s string) string {
+	b := []byte(s)
+	for i, j := 0, len(b)-1; i < j; {
+		if isVowels(b[i]) && isVowels(b[j]) {
+			b[i], b[j] = b[j], b[i]
+			i++
+			j--
+		} else if isVowels(b[i]) && !isVowels(b[j]) {
+			j--
+		} else if !isVowels(b[i]) && isVowels(b[j]) {
+			i++
+		} else {
+			i++
+			j--
+		}
+	}
+	return string(b)
+}
+
+func isVowels(s byte) bool {
+	if s == 'a' || s == 'e' || s == 'i' || s == 'o' || s == 'u' || s == 'A' || s == 'E' || s == 'I' || s == 'O' || s == 'U' {
+		return true
+	}
+	return false
+}
+
+func main() {
+	fmt.Println(reverseVowels("hello"))
+}

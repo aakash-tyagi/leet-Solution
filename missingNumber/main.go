f7453f5698492feb86a6b5dde7415e5de2e0803b
aakash <tyagi01aakash.com>
2021-09-07 09:21:03 +0530

    missing-number

---
+++ missingNumber/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func missingNumber(nums []int) int {
+
+	sort.Ints(nums)
+	fmt.Println(nums)
+
+	for i := 0; i < len(nums); i++ {
+		if i != nums[i] {
+			return i
+		}
+	}
+
+	return len(nums)
+
+}
+
+func main() {
+	a := []int{0, 1, 2, 4}
+	fmt.Println(missingNumber(a))
+}

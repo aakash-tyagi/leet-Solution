df7d4cebb57c9eba4a395b37f62a9beab3c17314
aakash <tyagi01aakash.com>
2021-09-08 07:15:02 +0530

    power-of-three

---
+++ power-of-three/main.go
@@ -0,0 +1,23 @@
+package main
+
+import "fmt"
+
+func isPowerOfThree(s int) bool {
+	for s >= 3 {
+		if s%3 == 0 {
+			s = s / 3
+			fmt.Println(s)
+			if s == 1 {
+				return true
+			}
+		} else {
+			return false
+		}
+	}
+
+	return false
+}
+
+func main() {
+	fmt.Println(isPowerOfThree(9))
+}

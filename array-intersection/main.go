d87391ff3870b5c9a04d52d87d5f2d696c4a78d5
aakash <tyagi01aakash.com>
2021-09-17 10:46:03 +0530

    array-intersection

--- array-intersection/main.go
+++ array-intersection/main.go
@@ -2,36 +2,26 @@ package main
 
 import (
 	"fmt"
-	"sort"
-)
-
-package main
-
-import (
-	"fmt"
 )
 
 func intersection(nums1 []int, nums2 []int) []int {
 	res := []int{}
 	m := make(map[int]int)
-	for _,j := range nums1{
-		m[j]++					// counting number of paticluar int in ARRAY
+	for _, j := range nums1 {
+		m[j]++ // counting number of paticluar int in ARRAY
 	}
-	
-	for _,j := range nums2{
+
+	for _, j := range nums2 {
 		if m[j] > 0 {
-			res = append(res,j) // creating array of intersecting number
-			m[j]  = 0
+			res = append(res, j) // creating array of intersecting number
+			m[j] = 0
 		}
-	
+
 	}
-	
+
 	return res
 
 }
-
-
-
 
 func main() {
 	a := []int{0, 1, 2, 2, 3, 4, 5, 4, 4}

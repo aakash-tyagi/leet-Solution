225868c9ca2d9d7c4ea99b640da0d5e1ad5eb979
aakash <tyagi01aakash.com>
2021-11-11 23:48:52 +0530

    longest substring

---
+++ longest-substring/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+)
+
+func lengthOfLongestSubstring(s string) int {
+    var i, maximum_len int = 0, 0
+    mymap := make(map[byte]int)
+    
+    for j := 0; j < len(s); j++ {
+        mymap[s[j]]++
+        for mymap[s[j]] == 2 && i < j {
+            mymap[s[i]]--
+            i++
+        }
+        
+        if maximum_len < j - i + 1 {
+            maximum_len = j - i + 1
+        }
+    }
+    
+    return maximum_len
+}
+
+func main() {
+	a := "abcabcbb"
+	fmt.Println(lengthOfLongestSubstring(a))
+}
